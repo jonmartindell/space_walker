f0f67ff12536cfa7390a50407ee0df4ced3bac85
Jonathan Martindell <jmartindell@covermymeds.com>
2020-04-22 20:00:23 -0400

    Updates

---
+++ precache-manifest.a3a9b97319a577a4ed12a9ef883f0488.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/space-walker/favicon.ico"
+  },
+  {
+    "revision": "7f1394811bcbdaaf025221c2315968f8",
+    "url": "/space-walker/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/space-walker/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/space-walker/manifest.json"
+  },
+  {
+    "revision": "082d15ab40a4464f6ae01ccb3f968ca8",
+    "url": "/space-walker/shuttle.png"
+  },
+  {
+    "revision": "01926a31f060fa2111f5",
+    "url": "/space-walker/static/css/main.a448614b.chunk.css"
+  },
+  {
+    "revision": "01926a31f060fa2111f5",
+    "url": "/space-walker/static/js/main.01926a31.chunk.js"
+  },
+  {
+    "revision": "15199530fadadb9d0b8a",
+    "url": "/space-walker/static/js/runtime~main.15199530.js"
+  },
+  {
+    "revision": "7aa09ec89c957a98c55b",
+    "url": "/space-walker/static/js/vendors~main.7aa09ec8.chunk.js"
+  }
+]);
