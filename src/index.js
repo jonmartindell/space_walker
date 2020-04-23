c12fe9f2e2504732e294c972038e4a71004bd65f
Jonathan Martindell <jmartindell@covermymeds.com>
2020-04-23 12:43:34 -0400

    show rounded yards

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
       Math.sin(dLon / 2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var d = R * c;
-  var yards = d * 1094.0;
-  return d.toString();
+  var yards = d * 1093.61;
+  return Math.round(yards).toString(); // return rounded yards
 }
 Number.prototype.toRad = function () {
   return (this * Math.PI) / 180;
